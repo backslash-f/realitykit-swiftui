f2c75ca8e7d6b9c7719b3140ea6e6b5a31664206
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-04-12 14:09:44 +0200

    Add remove entity capability to the representable

--- ModelPicker/View/ARViewRepresentable.swift
+++ ModelPicker/View/ARViewRepresentable.swift
@@ -14,6 +14,7 @@ struct ARViewRepresentable: UIViewRepresentable {
     // MARK: - Properties
 
     @Binding var modelConfirmedForPlacement: Model?
+    @Binding var shouldRemoveAllModels: Bool
 
     // MARK: - UIViewRepresentable
 
@@ -22,19 +23,26 @@ struct ARViewRepresentable: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: ARView, context: Context) {
-        guard let model = modelConfirmedForPlacement else { return }
-
-        if let modelEntity = model.modelEntity {
+        if shouldRemoveAllModels {
+            uiView.scene.anchors.forEach { anchor in
+                anchor.children.forEach { entity in
+                    if Model.modelNames.contains(entity.name) {
+                        entity.removeFromParent()
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                shouldRemoveAllModels = false
+            }
+        } else if let model = modelConfirmedForPlacement,
+                  let modelEntity = model.modelEntity {
             print("Adding model to scene: \(model.modelName)")
             let anchorEntity = AnchorEntity(plane: .any)
             anchorEntity.addChild(modelEntity .clone(recursive: true))
             uiView.scene.addAnchor(anchorEntity)
-        } else {
-            print("Unable to load modelEntity for: \(model.modelName)")
-        }
-
-        DispatchQueue.main.async {
-            modelConfirmedForPlacement = nil
+            DispatchQueue.main.async {
+                modelConfirmedForPlacement = nil
+            }
         }
     }
 }
